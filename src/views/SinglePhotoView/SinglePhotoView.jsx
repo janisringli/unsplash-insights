8e1d1153b62e231e248cbcdea550097408aeeaa2
Janis Ringli <janis.ringli@gmail.com>
2023-04-26 17:48:51 +0200

    add function to SinglePhotoView to get id from url using regex

--- src/views/SinglePhotoView/SinglePhotoView.jsx
+++ src/views/SinglePhotoView/SinglePhotoView.jsx
@@ -6,8 +6,26 @@ import { useParams } from "react-router-dom";
 function SinglePhotoView() {
     const [singlePhoto, setSinglePhoto] = useState({});
    // const { photoId } = useParams("93NY0dTeUYE");
- let photoId = "0D356Mbgs7o"
+//  let photoId = "0D356Mbgs7o"
+ const url = window.location.href
+
+        function extractPhotoName(url) {
+            const regex = /photo\/(.+)/;
+            const match = url.match(regex);
+            if (match && match.length > 1) {
+              const photoId = match[1];
+              return photoId;
+            } else {
+              return null;
+            }
+          }
+          
+          
+          const photoId = extractPhotoName(url);
+          console.log(photoId);
     useEffect(() => {
+        
+
         async function retrieveData() {
         const singlePhotoData = await getSinglePhotos(photoId);
         setSinglePhoto(singlePhotoData);
