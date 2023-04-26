c8d2e972d414f957f8d1c2d9dffc77c158c231b5
Janis Ringli <janis.ringli@gmail.com>
2023-04-27 00:28:17 +0200

    played around with api call

--- src/api/api.js
+++ src/api/api.js
@@ -1,102 +1,90 @@
-export async function getPhotos(username){
-    try {
-        const response = await fetch(
-          `https://api.unsplash.com/users/${username}/photos/?per_page=30`,
-          {
-            method: "GET",
-            headers: new Headers({
-                Authorization: 'Client-ID 8AaOzWIzBjToHkNNYtgTYyBlnDcNr4-A_uBGakwF5BI',
-            }),
-          }
-        );
-        const payload = await response.json();
-        console.log(payload)
-        return payload;
-
-      } catch (error) {
-        console.log(error);
-      }
-  return await response.json
-};
-
-export async function getSinglePhotos(photoId){
-  try {
-    console.log(photoId)
-      const response = await fetch(
-        `https://api.unsplash.com/photos/${photoId}`,
-        {
-          method: "GET",
-          headers: new Headers({
-              Authorization: 'Client-ID 8AaOzWIzBjToHkNNYtgTYyBlnDcNr4-A_uBGakwF5BI',
-          }),
-        }
-      );
-      const payload = await response.json();
-      console.log(payload)
-      return payload;
-
-    } catch (error) {
-      console.log(error);
-    }
-return await response.json
-};
-
-export async function getStats(username){
-  try {
-      const response = await fetch(
-        `https://api.unsplash.com/users/${username}/`,
-        {
-          method: "GET",
-          headers: new Headers({
-              Authorization: 'Client-ID 8AaOzWIzBjToHkNNYtgTYyBlnDcNr4-A_uBGakwF5BI',
-          }),
-        }
-      );
-      const payload = await response.json();
-      return payload;
-
-    } catch (error) {
-      console.log(error);
-    }
-  
-return await response.json
-};
-
-export async function getMonthlyStats(){
+export async function getPhotos(username) {
   try {
     const response = await fetch(
-      `https://api.unsplash.com/stats/monthly`,
+      `https://api.unsplash.com/users/${username}/photos/?per_page=50&&page=1`,
       {
         method: "GET",
         headers: new Headers({
-            Authorization: 'Client-ID 8AaOzWIzBjToHkNNYtgTYyBlnDcNr4-A_uBGakwF5BI',
+          Authorization:
+            "Client-ID 8AaOzWIzBjToHkNNYtgTYyBlnDcNr4-A_uBGakwF5BI",
+        }),
+      }
+    );
+    const payload = await response.json();
+    console.log(payload);
+    return payload;
+  } catch (error) {
+    console.log(error);
+  }
+  return await response.json;
+}
+
+export async function getSinglePhotos(photoId) {
+  try {
+    console.log(photoId);
+    const response = await fetch(`https://api.unsplash.com/photos/${photoId}`, {
+      method: "GET",
+      headers: new Headers({
+        Authorization: "Client-ID 8AaOzWIzBjToHkNNYtgTYyBlnDcNr4-A_uBGakwF5BI",
+      }),
+    });
+    const payload = await response.json();
+    console.log(payload);
+    return payload;
+  } catch (error) {
+    console.log(error);
+  }
+  return await response.json;
+}
+
+export async function getStats(username) {
+  try {
+    const response = await fetch(
+      `https://api.unsplash.com/users/${username}/`,
+      {
+        method: "GET",
+        headers: new Headers({
+          Authorization:
+            "Client-ID 8AaOzWIzBjToHkNNYtgTYyBlnDcNr4-A_uBGakwF5BI",
         }),
       }
     );
     const payload = await response.json();
     return payload;
-
   } catch (error) {
     console.log(error);
   }
-  return await respons.json
-};
-export async function getTotalStats(){
+
+  return await response.json;
+}
+
+export async function getMonthlyStats() {
   try {
-    const response = await fetch(
-      `https://api.unsplash.com/stats/total`,
-      {
-        method: "GET",
-        headers: new Headers({
-            Authorization: 'Client-ID 8AaOzWIzBjToHkNNYtgTYyBlnDcNr4-A_uBGakwF5BI',
-        }),
-      }
-    );
+    const response = await fetch(`https://api.unsplash.com/stats/monthly`, {
+      method: "GET",
+      headers: new Headers({
+        Authorization: "Client-ID 8AaOzWIzBjToHkNNYtgTYyBlnDcNr4-A_uBGakwF5BI",
+      }),
+    });
     const payload = await response.json();
     return payload;
-
   } catch (error) {
     console.log(error);
   }
-  return await respons.json
-};
+  return await respons.json;
+}
+export async function getTotalStats() {
+  try {
+    const response = await fetch(`https://api.unsplash.com/stats/total`, {
+      method: "GET",
+      headers: new Headers({
+        Authorization: "Client-ID 8AaOzWIzBjToHkNNYtgTYyBlnDcNr4-A_uBGakwF5BI",
+      }),
+    });
+    const payload = await response.json();
+    return payload;
+  } catch (error) {
+    console.log(error);
+  }
+  return await respons.json;
+}
