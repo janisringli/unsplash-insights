3e53592ae24447945bdc66abc45625f808d25510
Janis Ringli <janis.ringli@gmail.com>
2023-04-27 17:13:36 +0200

    edit api call getPhotos so it retrieves all pictures instead of only 30

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 export async function getPhotos(username) {
   try {
-    const response = await fetch(
-      `https://api.unsplash.com/users/${username}/photos/?per_page=50&&page=1`,
+    const userProfileResponse = await fetch(
+      `https://api.unsplash.com/users/${username}`,
       {
         method: "GET",
         headers: new Headers({
@@ -10,13 +10,32 @@ export async function getPhotos(username) {
         }),
       }
     );
-    const payload = await response.json();
-    console.log(payload);
-    return payload;
+    const userProfile = await userProfileResponse.json();
+    const totalPhotos = userProfile.total_photos;
+    const perPage = 30;
+    const totalPages = Math.ceil(totalPhotos / perPage);
+    let photos = [];
+
+    for (let page = 1; page <= totalPages; page++) {
+      const response = await fetch(
+        `https://api.unsplash.com/users/${username}/photos/?per_page=${perPage}&page=${page}`,
+        {
+          method: "GET",
+          headers: new Headers({
+            Authorization:
+              "Client-ID 8AaOzWIzBjToHkNNYtgTYyBlnDcNr4-A_uBGakwF5BI",
+          }),
+        }
+      );
+
+      const payload = await response.json();
+      photos = photos.concat(payload);
+    }
+
+    return photos;
   } catch (error) {
     console.log(error);
   }
-  return await response.json;
 }
 
 export async function getSinglePhotos(photoId) {
