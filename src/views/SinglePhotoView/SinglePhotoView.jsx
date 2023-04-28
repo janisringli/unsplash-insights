51c56f4d840770241ef0bd404d689531f542018d
Janis Ringli <janis.ringli@gmail.com>
2023-04-28 17:21:45 +0200

    start work on singlephotoview

--- src/views/SinglePhotoView/SinglePhotoView.jsx
+++ src/views/SinglePhotoView/SinglePhotoView.jsx
@@ -2,56 +2,43 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { getPhotos, getSinglePhotos } from "../../api/api";
 import { useParams } from "react-router-dom";
+import "./SinglePhotoView.css"
 
 function SinglePhotoView() {
-    const [singlePhoto, setSinglePhoto] = useState({});
-   // const { photoId } = useParams("93NY0dTeUYE");
-//  let photoId = "0D356Mbgs7o"
- const url = window.location.href
+  const [singlePhoto, setSinglePhoto] = useState({});
+  const { photoId } = useParams();
+  console.log(photoId);
 
-        function extractPhotoName(url) {
-            const regex = /photo\/(.+)/;
-            const match = url.match(regex);
-            if (match && match.length > 1) {
-              const photoId = match[1];
-              return photoId;
-            } else {
-              return null;
-            }
-          }
-          
-          
-          const photoId = extractPhotoName(url);
-          console.log(photoId);
-    useEffect(() => {
-        
-
-        async function retrieveData() {
-        const singlePhotoData = await getSinglePhotos(photoId);
-        setSinglePhoto(singlePhotoData);
-        }
-    
-        retrieveData();
-    }, []);
-    
-    return (
-        <div className="App">
-        <div>
-            <div>
-                <img
-                key={singlePhoto.id}
-                 src={singlePhoto?.urls?.small}
-                alt={singlePhoto.alt_description}
-                />
-                <p>id: {singlePhoto.id}</p>
-            </div>
-            <div className="evaluation-wrapper">
-                <div className="evaluation-content">
-                
-                </div>
-            </div>
-        </div>
-        </div>
-    );
+  useEffect(() => {
+    async function retrieveData() {
+      const singlePhotoData = await getSinglePhotos(photoId);
+      setSinglePhoto(singlePhotoData);
+      
     }
-    export default SinglePhotoView;
+
+    retrieveData();
+    
+  }, [photoId]);
+  return (
+    <div className="App">
+      <div className="single-photo-view-wrapper">
+        <div className="image-info">hello</div>
+        <div className="image-info">hello</div>
+        <div className="single-image-container">
+          <img className="single-image"
+            key={singlePhoto.id}
+            src={singlePhoto?.urls?.small}
+          />
+          <p className="unsplash-credit">Photo by <a className="link-to-unsplash" href={singlePhoto?.user?.links.html} target="_blank">{singlePhoto?.user?.first_name} {singlePhoto?.user?.last_name}</a> on <a className="link-to-unsplash" href={singlePhoto?.links?.html} target="_blank">Unsplash</a></p>
+        </div>
+        <div className="evaluation-wrapper">
+          <div>
+         
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SinglePhotoView;
