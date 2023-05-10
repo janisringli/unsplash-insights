ad12bfc24e9d8978efe4d18a274c2e86f9478b06
Janis Ringli <janis.ringli@gmail.com>
2023-05-10 15:46:26 +0200

    add Home to navigation and route it to /

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,40 +1,45 @@
 import React from 'react';
 import './Header.css';
 import { useState, useContext } from 'react';
-// import globals from '../../globals.js';
 import { UserContext } from '../../App.jsx';
 import { useNavigate } from 'react-router-dom';
 
 function Header() {
-    const {username ,setUsername} = useContext(UserContext);
-    const [tempUsername, setTempUsername] = useState("");
-    const navigate = useNavigate();
+  const { username, setUsername } = useContext(UserContext);
+  const [tempUsername, setTempUsername] = useState("");
+  const navigate = useNavigate();
 
-    function handleSubmit(event) {
-        event.preventDefault();
-        setUsername(tempUsername);
-        navigate(`/profile/${tempUsername}`);
+  function handleSubmit(event) {
+    event.preventDefault();
+    setUsername(tempUsername);
+    navigate(`/profile/${tempUsername}`);
+  }
 
-      }
-    
-      function handleChange(event) {
-        setTempUsername(event.target.value);
+  function handleChange(event) {
+    setTempUsername(event.target.value);
+  }
 
-      }
+  function handleHomeClick() {
+    navigate('/');
+  }
+
   return (
-    //search input field and search button that gets stored in a variable called username
     <div className="header-wrapper">
-        <div className="header-content">
-            <div className="header-title">Logo</div>
-            <div className='header-right'>
-                <div className="header-item">About</div>
-            <form className="header-search" onSubmit={handleSubmit}>
-                <input className="header-search-input" value={tempUsername} type="text" placeholder="Search for username" onChange={handleChange}/>
-                <button type="submit" className="header-search-button">Search</button>
-            </form>
-            </div>
+      <div className="header-content">
+        <div className="header-title">Logo</div>
+        <div className='header-right'>
+          <div className="item-container">
+          <div className="header-item" onClick={handleHomeClick}>Home</div>
+          <div className="header-item">About</div>
+          </div>
+          <form className="header-search" onSubmit={handleSubmit}>
+            <input className="header-search-input" value={tempUsername} type="text" placeholder="Search for username" onChange={handleChange}/>
+            <button type="submit" className="header-search-button">Search</button>
+          </form>
         </div>
+      </div>
     </div>
   );
 }
+
 export default Header;
